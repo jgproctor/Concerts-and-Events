264306ad0dc8b336984ad7d1886b5a0397026c9a
Jonathan Proctor <Jonathan.Proctor@vixxo.com>
2018-06-24 11:11:59 -0700

    Adding venue to concert info.

--- ConcertAPI.py
+++ ConcertAPI.py
@@ -8,14 +8,15 @@ import requests
 class City():
     
     Concerts = []
-    length = 0
+    eventsLength = 0
     name = ""
     event = ""
     
     def request(self):
         response = requests.get("https://app.ticketmaster.com/discovery/v2/events.json?apikey=cfCvyEeFFUCUox7laqXgfSeSX3Ac1zd3&city=" + self.name)
         data = response.json()
-        self.length = len(data["_embedded"]["events"])
+        self.eventsLength = len(data["_embedded"]["events"])
+        self.venuesLength = len(data["_embedded"]["events"][1]["_embedded"]["venues"])
         self.event = data["_embedded"]["events"]
         
         
@@ -27,23 +28,28 @@ class City():
     def printConcerts(self):
         k = 0
         for k in range(len(self.Concerts)):
-            print (self.Concerts[k].getName())
-            print (self.Concerts[k].getDate())
-            print (self.Concerts[k].getTime())
+            print()
+            print("Name: " + self.Concerts[k].getName())
+            print("Date: " + self.Concerts[k].getDate())
+            print("Time: " + self.Concerts[k].getTime())
+            print("Venue: " + self.Concerts[k].getVenue())
             print()
             
             
     def createEvents(self):
         i = 0
+        j = 0
         
-        for i in range(self.length):
+        for i in range(self.eventsLength):
+            
+            for j in range (len(self.event[i]["_embedded"]["venues"])):
+                con = Concert()
+                con.setName(self.event[i]["name"])
+                con.setDate(self.event[i]["dates"]["start"]["localDate"])
+                con.setTime(self.event[i]["dates"]["start"]["localTime"])
+                con.setVenue(self.event[i]["_embedded"]["venues"][j]["name"])
     
-            con = Concert()
-            con.setName(self.event[i]["name"])
-            con.setDate(self.event[i]["dates"]["start"]["localDate"])
-            con.setTime(self.event[i]["dates"]["start"]["localTime"])
-    
-            self.Concerts.append(con)
+                self.Concerts.append(con)
 
     def sortConcerts(self):
         self.Concerts = sorted(self.Concerts, key = lambda x: datetime.strptime(x.getDate(), '%Y-%m-%d'))
@@ -51,6 +57,7 @@ class City():
 class Concert():
         
     def setTime(self, time):
+        
         self.time = time
         
     def setName(self, name):
@@ -58,6 +65,9 @@ class Concert():
     
     def setDate(self, date):
         self.date = date
+        
+    def setVenue(self, venue):
+        self.venue = venue
     
     def getTime(self):
         return self.time
@@ -67,6 +77,9 @@ class Concert():
     
     def getDate(self):
         return self.date
+    
+    def getVenue(self):
+        return self.venue
         
 
 city = City()
